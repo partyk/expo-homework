67620ea72fb735980a226d377e5ec5e6173ba4a6
Bohdan Partyk <info@podebrady.org>
2018-11-15 19:20:54 +0100

    lesson 3 part 1

--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Text, SafeAreaView, StyleSheet, FlatList } from 'react-native'
 
 // components
 import { RoundedButton } from '../components'
@@ -13,12 +13,33 @@ const styles = StyleSheet.create({
   },
 })
 
+const beers = ['aaa', 'bbb', 'ccc'];
+
 export default class RootContainer extends React.PureComponent<null> {
+  state = {
+    count: 0,
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
+          {/*{beers.map((beer) => (*/}
+              {/*<Text>{beer}</Text>*/}
+          {/*))}*/}
+          <Text>{this.state.count}</Text>
+
+          <FlatList
+            data={beers}
+            renderItem={({item}) => <Text>{item}</Text>}
+            keyExtractor={item => item}
+          />
+
         <Text>Text!</Text>
-        <RoundedButton onPress={() => null}>Button text</RoundedButton>
+        <RoundedButton onPress={() => {
+          this.setState({
+              count: this.state.count+1
+          })
+        }}>Button text</RoundedButton>
       </SafeAreaView>
     )
   }
