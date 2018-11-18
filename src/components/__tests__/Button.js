006a1a0c1a47a337610df26f3eede357d0fd9a16
Bohdan Partyk <info@podebrady.org>
2018-11-18 12:44:31 +0100

    button test

--- src/components/__tests__/Button.js
+++ src/components/__tests__/Button.js
@@ -3,6 +3,14 @@ import renderer from 'react-test-renderer';
 
 // components
 import Button from '../Button';
+import {ButtonThemes} from '../../themes';
+
+test('Button primary renders correctly', () => {
+    const tree = renderer
+        .create(<Button onPress={() => null} theme={ButtonThemes.primary}>Button primary</Button>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
 
 test('Button secondary renders correctly', () => {
     const tree = renderer
